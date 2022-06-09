9e00e1eb1a98c61c79146851949dca1c3050cb6d
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-06-09 11:15:01 +0100

    setup components files & add HeroBanner with some text

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const HeroBanner = () => {
+    return (
+        <div className="hero-banner-container">
+            <div>
+                <p className="beats-solo">Small Text</p>
+                <h3>Mid Text</h3>
+                <img src={''} alt="headphones" className="hero-banner-image" />
+                <div>
+                    <Link href="/product/ID">
+                        <button type="button">Button Text</button>
+                    </Link>
+                    <div className="desc">
+                        <h5>Description</h5>
+                        <p>DESCRIPTION</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroBanner;
